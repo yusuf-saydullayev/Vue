3d0f1baeb1aa551055e744f00c59d2e51ab7ac75
yusuf-saydullayev <yusufsaydullayev@gmail.com>
2023-09-27 14:43:04 +0500

    fix

--- src/components/Form-Create.vue
+++ src/components/Form-Create.vue
@@ -6,14 +6,14 @@ import Button from '@/components/Custom-Button.vue'
 type ComponentProps = {
     createUser: ({ name, age }: createUserProps) => void
 }
-const props = defineProps<ComponentProps>();
+const { createUser } = defineProps<ComponentProps>();
 
 const userName = ref<string>('')
 const userAge = ref<number>(0)
 
 const onSubmit = () => {
     if (userName.value && userAge.value) {
-        props.createUser({ name: userName.value, age: userAge.value })
+        createUser({ name: userName.value, age: userAge.value })
         userName.value = ''
         userAge.value = 0
     }
@@ -26,9 +26,9 @@ const onSubmit = () => {
         <form @submit.prevent="onSubmit" class="flex flex-col gap-5 p-4 border-2 border-fuchsia-700">
             <input v-model.trim="userName" type="text" placeholder="Name"
                 class="px-3 py-2 bg-white border border-slate-300 rounded-md shadow-sm placeholder-slate-400" />
-            <input v-model.trim="userAge" type="number" placeholder="Age" min="1"
+            <input v-model.number="userAge" type="number" placeholder="Age" min="1"
                 class="px-3 py-2 bg-white border border-slate-300 rounded-md shadow-sm placeholder-slate-400" />
-            <Button>
+            <Button type="submit">
                 Add User
             </Button>
         </form>
